b22d36849c1e948e9b9737ef7c1ffdf52a5edd55
Kelvin Veras <kelvin.veras06@aluno.ifce.edu.br>
2025-08-25 23:51:20 +0000

    Alteracoes realizadas

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def hello():
 def get_aluno():
     return{"insira na rota após a '/' o nome do aluno"}
 
-@app.get("/aluno{nome_aluno}")
+@app.get("/aluno/{nome_aluno}")
 def get_aluno_by_name(nome_aluno: str):
     return{"o nome do aluno é": nome_aluno}
